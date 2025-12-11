ffa962b91fafc53d849b0021e57885550c48b59f
kevin <kevinthomassouza@gmail.com>
2025-12-11 13:42:58 -0300

    Atividade NestJS 31 e 32

---
+++ src/auth/constants/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+    secret: 'a670a3f6f6e57aa15c2248bcc5487315310d737c53c798ea9afdeae36ea51114',
+};
---
+++ src/auth/controllers/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from "@nestjs/common";
+import { AuthService } from "../services/auth.service";
+import { UsuarioLogin } from "../entities/usuariologin.entity";
+import { LocalAuthGuard } from "../guard/local-auth.guard";
+import { UsuarioResponse } from "../Interfaces/usuario-response.interface";
+
+
+@Controller('/usuarios')
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @UseGuards(LocalAuthGuard)
+    @Post('/login')
+    @HttpCode(HttpStatus.OK)
+    login(@Body() usuario: UsuarioLogin): Promise<UsuarioResponse> {
+        return this.authService.login(usuario);
+    }
+
+    
+}
